012ce3639711f39e7a2d5d3dd13eca7cdc31a6ba
estebane3 <47418174+estebane3@users.noreply.github.com>
2019-04-05 22:27:58 -0300

    Add files via upload

---
+++ Trabajo Practico Nº1 Programacion-Laboratorio/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operaciones.h"
+
+int main()
+{
+    int num1=0;
+    int num2=0;
+    int opciones;
+    int resultadosumar;
+    int resultadorestar;
+    int resuttadodividir;
+    int resultadomultiplicar;
+    int resultadofactorial;
+    int flag_1=0;
+    int flag_2=0;
+
+
+        printf("CALCULADORA\n");
+
+    do{
+
+         if(flag_1==0)
+            {
+                printf("\n1- Ingrese el primer operando ");
+            }
+            else
+                {
+                printf("\n1- Ingrese el primer operando A=%d ",num1);
+                }
+
+                if(flag_2==0)
+                    {
+                        printf("\n2- Ingrese el segundo operando \n");
+                }
+                else
+                {
+                    printf("\n2- Ingrese el segundo operando  B=%d \n",num2);
+                }
+                printf("3- Calcular todas las operaciones \n");
+                printf("4- Informar resultados \n");
+                printf("5- Salir \n");
+
+                printf("\nDigite su opcion: ");
+                scanf("%d" , &opciones);
+
+        switch(opciones)
+        {
+            case 1:
+                printf("1.ingrese el primer operando: \n");
+                scanf("%d", &num1);
+                printf("El primer operando es:%d \n", num1);
+                flag_1=1;
+            break;
+            case 2:
+                printf("2.ingrese el segundo operando: \n");
+                scanf("%d", &num2);
+                printf("El segundo operando es:%d \n", num2);
+                flag_2=1;
+            break;
+            case 3:
+                printf("3.Calculando todas las operaciones, Ingrese 4 para mostrarlo \n");
+            break;
+            case 4:
+                 if(flag_1==0 || flag_2==0)
+                {
+                    printf("Error. La operacion no se puede realizar porque falta ingresar los operando");
+                }else
+                {
+                printf("4.informar todos los resultados: \n");
+
+                resultadosumar= sumar(num1, num2);
+                resultadorestar= restar(num1, num2);
+                resuttadodividir= dividir(num1, num2);
+                resultadomultiplicar= multiplicar(num1, num2);
+                resultadofactorial= factorial(num1, num2);
+                }
+            break;
+            case 5:
+                printf("5.salir\n");
+            break;
+            default:
+                printf("Error, ingrese una opcion correcta\n\n");
+            break;
+        }
+    }
+    while(opciones != 5);
+
+    return 0;
+}
---
+++ Trabajo Practico Nº1 Programacion-Laboratorio/operaciones.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/** \brief Suma los enteros num1 y num2, devuelve el total
+ *
+ * \param   num1 primer operando entero
+ * \param   num2 segundo operando entero
+ * \return retorna el total de los 2 operando
+ *
+ */
+
+int sumar(int num1, int num2)
+{
+    int totalsuma;
+    printf("El resultado del calculo (A+B) es : %d \n" , num1 + num2);
+    return totalsuma;
+
+}
+
+/** \brief resta  los enteros num1 y num2, devuelve el total
+ *
+ * \param num1 primer operando entero
+ * \param num2 segundo operando entero
+ * \return retorna el total de la resta de los 2 operando
+ *
+ */
+
+
+int restar(int num1, int num2)
+{
+    int totalresta;
+    printf("El resultado de su calculo (A-B) es: %d \n" , num1 - num2);
+    return totalresta;
+
+}
+
+/** \brief multiplicacion de los enteros num1 y num2, devuelve el total
+ *
+ * \param num1 primer operando entero
+ * \param num2 segundo operando entero
+ * \return retorna el total de la multiplicacion
+ *
+ */
+int multiplicar(int num1, int num2)
+{
+    int totalmutiplicar;
+    printf("El resultado de su calculo (A*B) es: %d \n" , num1 * num2);
+    return totalmutiplicar;
+}
+
+/** \brief division de los enteros num1 y num2 con casting, devuelve el resutaldo o error en caso de dividir valores menor a 0
+ *
+ * \param num1 primer operando entero con casting (float)
+ * \param num2 segundo operando entero
+ * \return el total de la divicion de num1 y num2, en el caso que se divida por 0 envia error
+ *
+ */
+
+int dividir(int num1, int num2)
+{
+    int totaldividir;
+    if (num2 == 0)
+    {
+        printf("Error. no se pudo dividir por 0 \n");
+    }
+    else
+    {
+        printf("El resultado de su calculo (A/B) es : %.2f \n" ,(float)num1 / num2);
+    }
+    return totaldividir;
+
+
+}
+/** \brief factorial del operando entero num1 y operando entero num2
+ *
+ * \param num1 primer operando entero
+ * \param num2 segundo operando entero
+ * \return devuelve el resultado o en el caso que algun operando sea negativo indicara que no es posible hacer el factorial
+ *
+ */
+
+
+int factorial(int num1, int num2)
+{
+    int totalfactorial;
+    int i;
+    int i2;
+    int factorial_1=1;
+    int factorial_2=1;
+
+    if (num1 > 0)
+    {
+        for(i=1;i<=num1;i++)
+    {
+    factorial_1=factorial_1*i;
+
+    }
+    }
+    else
+    {
+        printf("El primer numero no tiene factorial \n");
+    }
+    if (num2 > 0)
+    {
+        for(i2=1;i2<=num2;i2++)
+    {
+    factorial_2=factorial_2*i2;
+    }
+    }
+    else
+    {
+        printf("El segundo numero no tiene factorial \n");
+    }
+    printf("El factorial de A es : %d\n\n El factorial de B es: %d\n\n",factorial_1,factorial_2);
+    return totalfactorial;
+}
+
